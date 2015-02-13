cf8e5ed772d25a7547ed3cd700ba5245123c9e47
SanketDG <sanketdasgupta@gmail.com>
2015-02-13 18:29:37 +0530

    configure settings for heroku

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -84,3 +85,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 SITE_URL = "http://localhost:8000"
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
