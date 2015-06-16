ffc32cc9109855f0b5b245b7c60fa42dc1508bac
SanketDG <sanketdasgupta@gmail.com>
2015-06-17 05:23:42 +0530

    add test for checking length of shortened url is smaller

--- djurl/tests.py
+++ djurl/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Url
+from .views import *
 
-# Create your tests here.
+
+class UrlShortener(TestCase):
+    def test_short_url_large(self):
+        url = "http://www.example.com"
+        Url(httpurl=url)
+        short_url = get_short_code()
+        self.assertLess(len(short_url), len(url))
