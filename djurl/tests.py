f5b30b6b7ca47b5c343946d1b4bf9d7389289e84
SanketDG <sanketdasgupta@gmail.com>
2015-06-23 22:34:42 +0530

    add test to check original url is equal to httpurl in the Model

--- djurl/tests.py
+++ djurl/tests.py
@@ -6,6 +6,22 @@ from .views import *
 class UrlShortener(TestCase):
     def test_short_url_large(self):
         url = "http://www.example.com"
-        Url(httpurl=url)
         short_url = get_short_code()
         self.assertLess(len(short_url), len(url))
+
+    def test_recover_original_url(self):
+        url = "http://www.example.com"
+        u = Url(httpurl=url)
+        self.assertEqual(url, str(u))
+
+    # def test_short_code_duplication(self):
+    #     url = "http://www.example.com"
+    #     u = Url(httpurl=url)
+    #     sc = shorten_url(u.short_id)
+    #     self.assertEqual(u.short_id, sc)
+
+    # def test_short_id_redirection(self):
+    #     url = "http://www.example.com"
+    #     u = Url(httpurl=url)
+    #     redirect_original(short_id=u.short_id)
+    #     self.assertRedirects()
