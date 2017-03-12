d89f6cc42d3d9add480e1c34f9b0959dc5fe498a
Sanket Dasgupta <sanketdasgupta@gmail.com>
2017-03-12 16:43:48 +0530

    Update all the URLs to conform to the new API

--- djurl/urls.py
+++ djurl/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from . import views
 
-urlpatterns = patterns('djurl.views',
-                       url(r'^$', 'index', name='home'),
-                       # for our home/index page
+urlpatterns = [
+   url(r'^$', views.index, name='home'),
+   # for our home/index page
 
-                       url(r'^(?P<short_id>\w{6})$',
-                           'redirect_original', name='redirect_original'),
-                       # when short URL is requested it redirects to original
-                       # URL
+   url(r'^(?P<short_id>\w{6})$',
+       views.redirect_original, name='redirect_original'),
+   # when short URL is requested it redirects to original
+   # URL
 
-                       url(r'^makeshort/$', 'shorten_url', name='shorten_url'),
-                       # this will create a URL's short id and return the short
-                       # URL
-                       )
+   url(r'^makeshort/$', views.shorten_url, name='shorten_url'),
+   # this will create a URL's short id and return the short
+   # URL
+]
--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
-                       # if the URL pattern match /admin/ then open up admin
-                       # panel
+urlpatterns = [
+   url(r'^admin/', admin.site.urls),
+   # if the URL pattern match /admin/ then open up admin
+   # panel
 
-                       url(r'', include(
-                           'djurl.urls', namespace='djurl')),
-                       # if anything rather then /admin/ then it will look for
-                       # djurl/urls
-                       )
+   url(r'', include(
+       'djurl.urls', namespace='djurl')),
+   # if anything rather then /admin/ then it will look for
+   # djurl/urls
+
+]
